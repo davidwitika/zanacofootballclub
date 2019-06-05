8aead28c273a4c3257b493b314196f1887dfadbf
Eddy Shivers <eddymwanda@gmail.com>
2019-06-05 21:41:20 +0200

    Updated plugin menu

--- plugins/emedia/teammanagement/controllers/Matches.php
+++ plugins/emedia/teammanagement/controllers/Matches.php
@@ -1,19 +1,17 @@
 <?php namespace emedia\TeamManagement\Controllers;
 
-use Backend\Classes\Controller;
 use BackendMenu;
+use Backend\Classes\Controller;
 
-class Matches extends Controller
-{
-    public $implement = [        'Backend\Behaviors\ListController',        'Backend\Behaviors\FormController',        'Backend\Behaviors\ReorderController'    ];
-    
-    public $listConfig = 'config_list.yaml';
-    public $formConfig = 'config_form.yaml';
-    public $reorderConfig = 'config_reorder.yaml';
+class Matches extends Controller {
+	public $implement = ['Backend\Behaviors\ListController', 'Backend\Behaviors\FormController', 'Backend\Behaviors\ReorderController'];
 
-    public function __construct()
-    {
-        parent::__construct();
-        BackendMenu::setContext('emedia.TeamManagement', 'main-menu-item', 'side-menu-item4');
-    }
+	public $listConfig = 'config_list.yaml';
+	public $formConfig = 'config_form.yaml';
+	public $reorderConfig = 'config_reorder.yaml';
+
+	public function __construct() {
+		parent::__construct();
+		BackendMenu::setContext('emedia.TeamManagement', 'main-menu-item', 'side-menu-item4');
+	}
 }
